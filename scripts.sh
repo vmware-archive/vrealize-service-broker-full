f38282ad18fc02e10bd7edbbae8f1b964892dc2f
JaredGordon <jgordon@pivotal.io>
2015-10-19 16:05:03 -0400

    track request workflow for async processes.

--- scripts.sh
+++ scripts.sh
@@ -2,7 +2,7 @@
 
 #token=$(curl --insecure -H "Accept: application/json" -H 'Content-Type: application/json' --data '{"username":"vdude01@vra.lab","password":"P1v0t4l!","tenant":"lab"}' https://vra.vra.lab/identity/api/tokens| jsawk 'return this.id')
 
-token=MTQ0NDgzMjU3MTU5NjpmODYxMDgzZWU4YTlhNzM3ZjY0Mzp0ZW5hbnQ6bGFidXNlcm5hbWU6dmR1ZGUwMUB2cmEubGFiZXhwaXJhdGlvbjoxNDQ0OTE4OTcxNTk2OjdkNjFmN2M2ODY2ZmRjNjQ2ZjFjNjA5YjExYjE0ZjBiYmViYWJlYjg4MDQwMmM3OTg0MmQzMDYzODI1NDQ5MDZhMGJjY2ZmMWJkYTJhOTFlYTMyMWU2NWNhM2JkMGI5MWJjZjA0ZTQ3OTI5MzczZjk1ODNjYjQ1NjVmNjhmZTY1
+token=MTQ0NTI4MzM3MTE5MDowMDc5MmM1M2NhODg4YWI4ODM1Njp0ZW5hbnQ6bGFidXNlcm5hbWU6dmR1ZGUwMUB2cmEubGFiZXhwaXJhdGlvbjoxNDQ1MzY5NzcxMTkwOmJkYTFkMjk4ZmE5MzY5NmMzNzIxZTI5YWRlYjk4ZTU2YzViZmNjMWUwYmFkZWFmYmFmZTAxMWUxODY1ODE1YmE1NjU1NWQxNzcyYmMyN2QxZjJjMGNmYWFkYzFhMGNkZjYzMTFmZjk5ZDE5NDQxZTI3ZjgwYTBmN2ZmOWRjMGFl
 
 #curl --insecure -H "Content-Type: application/json" -H "Authorization: Bearer $token" https://vra.vra.lab/identity/api/authorization/tenants/lab/principals/vdude01@vra.lab/roles
 
@@ -16,4 +16,13 @@ token=MTQ0NDgzMjU3MTU5NjpmODYxMDgzZWU4YTlhNzM3ZjY0Mzp0ZW5hbnQ6bGFidXNlcm5hbWU6dm
 
 #curl --insecure -X GET -H "Content-Type: application/json" -D headers.out -H "Authorization: Bearer $token" https://vra.vra.lab/catalog-service/api/consumer/entitledCatalogItems/e06ff060-dc7a-4f46-a7a7-c32c031fa31e/requests/template
 
-curl --insecure -X GET -H "Content-Type: application/json" -D headers.out -H "Authorization: Bearer $token" https://vra.vra.lab/catalog-service/api/consumer/requests/5c09a0f6-a19f-4ce9-904a-8f3bf8242ddc
+#curl --insecure -X GET -H "Content-Type: application/json" -D headers.out -H "Authorization: Bearer $token" https://vra.vra.lab/catalog-service/api/consumer/requests/9ca10dee-730e-486a-9138-d8aade4913e2/resourceViews?withExtendedData=true&withOperations=true&limit=100
+
+#curl --insecure -X GET -H "Content-Type: application/json" -D headers.out -H "Authorization: Bearer $token" https://vra.vra.lab/catalog-service/api/consumer/requests
+
+#curl --insecure -X GET -H "Content-Type: application/json" -D headers.out -H "Authorization: Bearer $token" https://vra.vra.lab/catalog-service/api/consumer/resourceViews?limit=100
+
+#curl --insecure -X GET -H "Content-Type: application/json" -D headers.out -H "Authorization: Bearer $token" https://vra.vra.lab/catalog-service/api/consumer/resourceViews/ba73b71f-e65a-4c22-8b85-33ddcd4fd551?withExtendedData=true&withOperations=true&limit=100
+
+
+curl --insecure -X GET -H "Content-Type: application/json" -D headers.out -H "Authorization: Bearer $token" https://vra.vra.lab/catalog-service/api/consumer/resources/d591e58d-b2cf-4061-aec1-7f41168b7a6d/actions/fe9af618-f21d-47a2-bebc-62d5914f6e6c/requests/template
---
+++ src/test/java/org/cloudfoundry/community/servicebroker/vrealize/VrServiceInstanceTest.java
@@ -0,0 +1,96 @@
+package org.cloudfoundry.community.servicebroker.vrealize;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.cloudfoundry.community.servicebroker.model.CreateServiceInstanceRequest;
+import org.cloudfoundry.community.servicebroker.model.OperationState;
+import org.cloudfoundry.community.servicebroker.vrealize.domain.VrServiceInstance;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = { Application.class })
+public class VrServiceInstanceTest {
+
+	@Test
+	public void testStates() {
+		CreateServiceInstanceRequest req = new CreateServiceInstanceRequest();
+
+		// new create request
+		VrServiceInstance si = VrServiceInstance.create(req, "12345");
+
+		assertTrue(si.isCurrentOperationCreate());
+		assertFalse(si.isCurrentOperationDelete());
+		assertFalse(si.isCurrentOperationSuccessful());
+		assertTrue(si.isInProgress());
+		assertEquals("12345", si.getServiceInstanceLastOperation()
+				.getDescription());
+
+		// failed create request
+		VrServiceInstance.update(si, OperationState.FAILED);
+
+		assertTrue(si.isCurrentOperationCreate());
+		assertFalse(si.isCurrentOperationDelete());
+		assertFalse(si.isCurrentOperationSuccessful());
+		assertFalse(si.isInProgress());
+		assertEquals("12345", si.getServiceInstanceLastOperation()
+				.getDescription());
+
+		// succeeded create request
+		VrServiceInstance.update(si, OperationState.SUCCEEDED);
+
+		assertTrue(si.isCurrentOperationCreate());
+		assertFalse(si.isCurrentOperationDelete());
+		assertTrue(si.isCurrentOperationSuccessful());
+		assertFalse(si.isInProgress());
+		assertEquals("12345", si.getServiceInstanceLastOperation()
+				.getDescription());
+
+		// new delete request added to existing si
+		si = VrServiceInstance.delete(si, "23456");
+
+		assertFalse(si.isCurrentOperationCreate());
+		assertTrue(si.isCurrentOperationDelete());
+		assertFalse(si.isCurrentOperationSuccessful());
+		assertTrue(si.isInProgress());
+		assertEquals("23456", si.getServiceInstanceLastOperation()
+				.getDescription());
+		assertEquals("23456",
+				si.getMetadata().get(VrServiceInstance.VrKey.DELETE_REQUEST_ID));
+		assertEquals("12345",
+				si.getMetadata().get(VrServiceInstance.VrKey.CREATE_REQUEST_ID));
+
+		// delete failed
+		si = VrServiceInstance.update(si, OperationState.FAILED);
+
+		assertFalse(si.isCurrentOperationCreate());
+		assertTrue(si.isCurrentOperationDelete());
+		assertFalse(si.isCurrentOperationSuccessful());
+		assertFalse(si.isInProgress());
+		assertEquals("23456", si.getServiceInstanceLastOperation()
+				.getDescription());
+		assertEquals("23456",
+				si.getMetadata().get(VrServiceInstance.VrKey.DELETE_REQUEST_ID));
+		assertEquals("12345",
+				si.getMetadata().get(VrServiceInstance.VrKey.CREATE_REQUEST_ID));
+
+		// delete succeeded
+		si = VrServiceInstance.update(si, OperationState.SUCCEEDED);
+
+		assertFalse(si.isCurrentOperationCreate());
+		assertTrue(si.isCurrentOperationDelete());
+		assertTrue(si.isCurrentOperationSuccessful());
+		assertFalse(si.isInProgress());
+		assertEquals("23456", si.getServiceInstanceLastOperation()
+				.getDescription());
+		assertEquals("23456",
+				si.getMetadata().get(VrServiceInstance.VrKey.DELETE_REQUEST_ID));
+		assertEquals("12345",
+				si.getMetadata().get(VrServiceInstance.VrKey.CREATE_REQUEST_ID));
+	}
+
+}
