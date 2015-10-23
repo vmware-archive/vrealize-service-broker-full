e2bdbfb1584573cd8a093e472eba9466d869e1b0
JaredGordon <jgordon@pivotal.io>
2015-10-23 15:29:25 -0400

    mongify.

--- src/test/java/org/cloudfoundry/community/servicebroker/vrealize/TestConfig.java
+++ src/test/java/org/cloudfoundry/community/servicebroker/vrealize/TestConfig.java
@@ -5,23 +5,21 @@ import java.net.UnknownHostException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import org.cloudfoundry.community.servicebroker.exception.ServiceBrokerException;
+import org.cloudfoundry.community.servicebroker.model.CreateServiceInstanceBindingRequest;
 import org.cloudfoundry.community.servicebroker.model.CreateServiceInstanceRequest;
+import org.cloudfoundry.community.servicebroker.model.ServiceInstanceBinding;
 import org.cloudfoundry.community.servicebroker.vrealize.domain.Creds;
 import org.cloudfoundry.community.servicebroker.vrealize.persistance.VrServiceInstance;
-import org.cloudfoundry.community.servicebroker.vrealize.persistance.VrServiceInstanceRepository;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 
 @Configuration
-@EnableMongoRepositories(basePackages = "org.cloudfoundry.community.servicebroker.vrealize.persistance", includeFilters = @ComponentScan.Filter(value = { VrServiceInstanceRepository.class }, type = FilterType.ASSIGNABLE_TYPE))
 public class TestConfig {
 
 	public static final String SD_ID = "e06ff060-dc7a-4f46-a7a7-c32c031fa31e";
@@ -61,7 +59,33 @@ public class TestConfig {
 	}
 
 	public static VrServiceInstance getServiceInstance() {
-		return VrServiceInstance.create(getServiceInstanceRequest(), "12345");
+		VrServiceInstance si = VrServiceInstance.create(
+				getServiceInstanceRequest(), "12345");
+		si.getParameters().put(VrServiceInstance.SERVICE_TYPE, "mysql");
+		si.getParameters().put(VrServiceInstance.DB_ID, "aDB");
+		si.getParameters().put(VrServiceInstance.HOST, "aHost");
+		si.getParameters().put(VrServiceInstance.PASSWORD, "secret");
+		si.getParameters().put(VrServiceInstance.PORT, "1234");
+		si.getParameters().put(VrServiceInstance.USER_ID, "aUser");
+		return si;
+	}
+
+	public static CreateServiceInstanceBindingRequest getBindingRequest() {
+		VrServiceInstance si = getServiceInstance();
+		CreateServiceInstanceBindingRequest req = new CreateServiceInstanceBindingRequest(
+				si.getServiceDefinitionId(), si.getPlanId(),
+				si.getOrganizationGuid(), si.getParameters());
+		req.withBindingId("98765");
+		req.withServiceInstanceId(si.getServiceInstanceId());
+		return req;
+	}
+
+	public static ServiceInstanceBinding getServiceInstanceBinding()
+			throws ServiceBrokerException {
+		CreateServiceInstanceBindingRequest req = getBindingRequest();
+		return new ServiceInstanceBinding(req.getBindingId(),
+				req.getServiceInstanceId(), getServiceInstance()
+						.getCredentials(), null, req.getAppGuid());
 	}
 
 }
