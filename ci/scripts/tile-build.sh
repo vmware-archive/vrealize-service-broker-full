7ed30c6c3ada412a2db8c18094b328d66d4a0270
JaredGordon <jgordon@pivotal.io>
2016-04-25 15:42:05 -0400

    ci tile build.

--- ci/scripts/tile-build.sh
+++ ci/scripts/tile-build.sh
@@ -4,12 +4,23 @@ set -e -x
 
 apt-get update
 apt install maven -y --fix-missing
+apt-get install zip -y --fix-missing
+apt-get install ruby -y --fix-missing
+apt-get install ruby-bundler -y --fix-missing
+gem install bosh_cli --no-ri --no-rdoc
+
+wget "https://bootstrap.pypa.io/get-pip.py" -O /dev/stdout | python
+pip install mkdocs
 
 
 TILE_GEN_DIR=$1
 SOURCE_DIR=$2
 HISTORY_DIR=$3
 TARGET_DIR=$4
+
+cd TILE_GEN_DIR
+pip install -r requirements.txt
+
 
 BIN_DIR="$( cd "${TILE_GEN_DIR}/bin" && pwd )"
 
